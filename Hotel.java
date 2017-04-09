b9171105f2fe2784dccfeb6ae8dc71e9d7aa9d9f
Satheesh Kumar <satheesh.101097@gmail.com>
2017-04-09 21:54:05 +0530

    Add files via upload

---
+++ Hotel.java
@@ -0,0 +1,98 @@
+import java.io.*;
+import java.util.*;
+
+class BreakFast{
+	private int break_items[][] = {
+		{50,25},
+		{20,10},
+		{40,67},
+		{45,67}
+	};
+
+	private String items_name[] = {
+		"Idly",
+		"Vada",
+		"Dosa",
+		"Puri"
+	};
+
+	private int total;
+
+	void BreakFast() {
+		total = 0; 
+	}
+
+	public void print_items(){
+		System.out.println("      Items Available For BreakFast      ");
+		System.out.println("=========================================");
+		System.out.println("S.no   |   Item   |  Price  |  Available |");
+		System.out.println("=========================================");
+		for(int i=0;i<4;i++){
+			System.out.println((i+1)+"      | "+items_name[i]+"     | "+break_items[i][1]+"      | "+break_items[i][0]+"         |");
+			if(i<3){
+				System.out.println("-----------------------------------------|");
+			}
+		}
+		System.out.println("=========================================");
+	}
+
+	public void purchase() throws Exception{
+		int sno,qty;
+		Scanner M = new Scanner(System.in);
+		System.out.println("Enter the Serial Number:");
+		sno = M.nextInt();
+		if(sno > 5 || sno < 1){
+			throw new ArithmeticException("The Serial Number Is Not A Valid One");
+		}
+		System.out.println("Enter the Quantity:");
+		qty = M.nextInt();
+		if(break_items[sno-1][0] < qty){
+			throw new ArrayIndexOutOfBoundsException("Stock Is Very Less");
+		}
+		break_items[sno-1][0] -= qty;
+		total+=(break_items[sno-1][1]*qty);
+		System.out.println("Item Added :)\n\n");
+	}
+
+	public void generate_bill(){
+		System.out.println("Amount :"+total);
+	}
+}
+
+class Hotel{
+	public static void main(String as[]){
+		BreakFast b = new BreakFast();
+		int ch=1;
+		Scanner M = new Scanner(System.in);
+		b.print_items();
+		while(ch != 4){
+			System.out.println("1.Add Item\n2.Bill\n3.Stock\n4.Exit");
+			ch = M.nextInt();
+			switch(ch){
+				case 1:
+					try{
+						b.purchase();
+					}
+					catch(ArithmeticException e){
+						System.out.println("The Serial Number Is Not A Valid One");
+					}
+					catch(ArrayIndexOutOfBoundsException e){
+						System.out.println("Stock Is Very Less");
+					}
+					catch(Exception e){
+						System.out.println("Some Error Occured In Our System");
+					}
+					break;
+				case 2:
+					System.out.println("Generating Bill");
+					b.generate_bill();
+					break;
+				case 3:
+					b.print_items();
+					break;
+				default:
+					System.out.println("Enter A Valid Choice");
+			}
+		}
+	}
+}
